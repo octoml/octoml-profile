9afec5005dba4a9cbd13232dc0ce2d899a60b7f6
An Wang <anwang2009@gmail.com>
2023-03-22 12:55:09 -0700

    no accelerate

--- examples/distilbert.py
+++ examples/distilbert.py
@@ -10,10 +10,10 @@ def predict(input: str):
   predicted_class_id = logits.argmax().item()
   return model.config.id2label[predicted_class_id]
 
-  examples = [
-    "Hello, world!",
-    "Nice to meet you",
-    "My dog is cute",
-  ]
-  for s in examples:
-      predict(s)
+examples = [
+  "Hello, world!",
+  "Nice to meet you",
+  "My dog is cute",
+]
+for s in examples:
+    predict(s)

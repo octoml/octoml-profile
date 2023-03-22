e9984f360e0a70fcf58bf5f52331437f2761b4d8
An Wang <anwang2009@gmail.com>
2023-03-22 13:32:50 -0700

    no octoml profile

--- examples/distilbert.py
+++ examples/distilbert.py
@@ -1,10 +1,8 @@
 from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
-from octoml_profile import accelerate, remote_profile
 
 tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
 model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased")
 
-@accelerate
 def predict(input: str):
   inputs = tokenizer(input, return_tensors="pt")
   logits = model(**inputs).logits
@@ -12,11 +10,10 @@ def predict(input: str):
   predicted_class_id = logits.argmax().item()
   return model.config.id2label[predicted_class_id]
 
-with remote_profile():
-    examples = [
-      "Hello, world!",
-      "Nice to meet you",
-      "My dog is cute",
-    ]
-    for s in examples:
-        predict(s)
+examples = [
+  "Hello, world!",
+  "Nice to meet you",
+  "My dog is cute",
+]
+for s in examples:
+    predict(s)
